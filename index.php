5deb7bdcd9a905b9405b61480525174370d01fc1
BoDVa <dmitry@bodva.dp.ua>
2011-06-14 22:02:59 +0300

    modified index.php add ":-P" smile

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		echo "<h1>Hello world!</h1>";
 		echo "<h2>My name is Vova! I know 3 words.</h2>";
                 for ($i=1; $i<=4; $i++) {
-                    echo "$i) Say \"Go to hell\""."<br>";
+                    echo "$i) Say \"Go to hell\" :-P"."<br>";
                 }
 		include ("about.txt");
 	?>
