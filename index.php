08a3d6f97637cafe5aa179d9b0120186cdbcbb6b
BoDVa <BoDVa@www.192.168.137.31>
2011-06-14 21:42:13 +0300

    modifiede index.php add /* for */ 	print /* Go to hell */

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 	<?php
 		echo "<h1>Hello world!</h1>";
 		echo "<h2>My name is Vova! I know 3 words.</h2>";
+                for ($i=1; $i<=4; $i++) {
+                    echo "$i) Say \"Go to hell\""."<br>";
+                }
 		include ("about.txt");
 	?>
 </body>
